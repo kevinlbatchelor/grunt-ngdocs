636fc3a3ceee669347a51ac9e63b0374eae28dc6
kbatchelor <kbatchelor@insidesales.com>
2015-09-24 15:13:43 -0600

    commented out a line that was breaking stuff according to the interwebs

--- src/templates/js/angular-bootstrap-prettify.js
+++ src/templates/js/angular-bootstrap-prettify.js
@@ -205,7 +205,7 @@ directive.ngEmbedApp = ['$templateCache', '$browser', '$rootScope', '$location',
         $provide.value('$anchorScroll', angular.noop);
         $provide.value('$browser', $browser);
         $provide.value('$sniffer', $sniffer);
-        $provide.value('$animate', $animate);
+//        $provide.value('$animate', $animate);
         $provide.provider('$location', function() {
           this.$get = ['$rootScope', function($rootScope) {
             docsRootScope.$on('$locationChangeSuccess', function(event, oldUrl, newUrl) {
